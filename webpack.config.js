c600aa9d2f8ff44d97519bcb25ead5b34d1e4251
honey <honey@topsinfosolutions.com>
2020-04-14 11:36:27 +0530

    sample-code-serverless

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const slsw = require('serverless-webpack');
+const path = require("path");
+const slsw = require("serverless-webpack");
 // const WebpackPluginCopy = require("inq-webpack-plugin-copy");
 module.exports = {
   entry: slsw.lib.entries,
-  target: 'node',
-  mode: 'production',
+  target: "node",
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
-        loaders: ['babel-loader'],
+        loaders: ["babel-loader"],
         include: __dirname,
         exclude: /node_modules/,
       },
@@ -19,25 +19,23 @@ module.exports = {
       // },
       {
         test: /\.node$/,
-        loaders: ['node-loader'],
+        loaders: ["node-loader"],
       },
       {
         test: /\.pem$/i,
-        loaders: ['raw-loader'],
-      }
-    ]
+        loaders: ["raw-loader"],
+      },
+    ],
   },
   externals: [
     (function () {
-      var IGNORES = [
-        'electron', 'pg-native'
-      ];
+      var IGNORES = ["electron", "pg-native"];
       return function (context, request, callback) {
         if (IGNORES.indexOf(request) >= 0) {
           return callback(null, "require('" + request + "')");
         }
         return callback();
       };
-    })()
-  ]
+    })(),
+  ],
 };
